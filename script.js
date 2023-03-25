400791a3d5bca0e03f383624b0e7e6d07a3047a5
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-25 11:28:21 -0500

    1 file modified

--- script.js
+++ script.js
@@ -195,5 +195,5 @@ console.log(todoJSON);
 
 for (let x = 0; x < 10; x++) {
   // anything in here is going to run until x is greater than or equal to ten
-  console.log(x);
+  console.log(`The For Loop Number Is: ${x}`);
 }
