5e3a036627843064285138f0fab16a53827aa7e0
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-24 22:01:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javascript-Course
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-7sd6pf)
---
+++ script.js
@@ -0,0 +1,31 @@
+// Variable let, const, We don't use var anymore
+
+// Use const unless you need to resign a variable
+
+// let is redefinable
+/*
+let age = 40;
+age = 20;
+
+console.log(age);
+*/
+
+// Const is not redefinanle, You have to give const a valu
+/*
+const age = 40;
+age = 20;
+
+console.log(age);
+*/
+
+// String, Number, Boolean, Null, Undefined
+
+const name = 'Matthew';
+const age = ' 14';
+const isHappy = true;
+const x = null;
+const z = undefined;
+//same as undefined
+let a;
+
+console.log(typeof name);
