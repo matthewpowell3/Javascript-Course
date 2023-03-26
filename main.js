57883ddb11128442048d74bf5ebe8fb58a46b8d1
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-26 11:35:44 -0500

    1 file modified, 1 added

---
+++ main.js
@@ -0,0 +1,9 @@
+// DOM Selector
+
+// Single element
+const form = document.getElementById('my-form');
+const container = document.querySelector('.container');
+
+// Multiple element
+
+console.log(document.querySelectorAll('.item'));
