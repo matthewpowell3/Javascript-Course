08cbd127dbf476c5a4834a509baa07c7cf29a71a
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-26 11:59:17 -0500

    1 file modified

--- main.js
+++ main.js
@@ -13,3 +13,27 @@ const container = document.querySelector('.container');
 // console.log(document.getElementsByClassName('item'));
 // tags
 // console.log(document.getElementsByTagName('li'));
+
+const ul = document.querySelector('.items');
+
+// One of the method
+// ul.remove();
+
+// Remove the last element child of ul
+// ul.lastElementChild.remove();
+
+// Remove the first element child of ul
+// ul.firstElementChild.remove();
+
+// Changes the text content of the first ul element
+ul.firstElementChild.textContent = 'User One';
+// Selects the second one
+ul.children[1].innerText = 'User Two';
+ul.lastElementChild.innerHTML = '<h3>User Three</h3>';
+
+//Styles
+
+const btn = document.querySelector('.btn');
+
+btn.style.background = 'red';
+btn.style.fontWeight = '900';
